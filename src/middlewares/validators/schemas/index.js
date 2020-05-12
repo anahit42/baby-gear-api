3d9e446f73560e84aa4b204865faf3769cc09dba
Anahit Vardanyan <vardanian.anahit@gmail.com>
2020-05-12 21:07:42 +0400

    Add validator schema files

--- src/middlewares/validators/schemas/index.js
+++ src/middlewares/validators/schemas/index.js
@@ -1,1 +1,5 @@
+module.exports.CategorySchemas = require('./category-schemas');
+module.exports.FavoriteSchemas = require('./favorite-schemas');
+module.exports.OrderSchemas = require('./order-schemas');
+module.exports.ProductSchemas = require('./product-schemas');
 module.exports.UserSchemas = require('./user-schemas');
