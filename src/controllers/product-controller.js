6287b029531d34e0f476edae93849533d632e91a
AnahitKhachatryan <anahitxachatryan11@gmail.com>
2020-05-14 20:49:29 +0400

    Add endpoint for products list

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,4 +1,38 @@
-// eslint-disable-next-line no-unused-vars
-const { ProductModel } = require('../models');
+function pageinatedResults(model) {
+    return async(req,res,next) =>{
+     const {page, limit} = req.query;
+   try {
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const results = {}
+    const count = await model.countDocuments();
+    const countPage = Math.ceil(count/limit);
+    results.countPage = countPage;
+    results.count = count;
+    if(endIndex < count) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      }
+    }
+    if(startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      }
+    }
+    results.results = await model.find().limit(parseInt(limit)).skip(startIndex).exec();
+    res.pageinatedResults = results;
+    return res.status(200).json({
+     results
+     });
 
-module.exports = {};
+   } catch(eroor) {
+    return next(error);
+   }  
+    }
+  }
+  
+module.exports = {
+    pageinatedResults
+};
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { ProductModel } = require('../models');
+const { pageinatedResults} = require('../controllers/product-controller');
+
+router.get('/products', pageinatedResults(ProductModel))
 module.exports = router;
