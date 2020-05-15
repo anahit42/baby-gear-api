f9f6a3ae293a78b754c56c6fa28ee15a0f998409
Lilit <lilit@inone.am>
2020-05-15 18:12:59 +0400

    Add endpoint for listing user orders

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -1,4 +1,18 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 const { OrderModel } = require('../models');
 
-module.exports = {};
+async function getOrders(req, res, next){
+  const { limit, offset } = req.params;
+  try{
+    return res.status(200).json({
+      orders: await OrderModel.find().limit(limit).skip(offset).exec()
+    }); 
+  }
+  catch(error){
+    next(error);
+  }
+}
+
+module.exports = {
+  getOrders
+};
--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { getOrders } = require('../controllers/order-controller');
+
+router.get('/', getOrders);
+
 module.exports = router;
