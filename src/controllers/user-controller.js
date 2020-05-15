0b95d0cab4eb4686a11397a0b413542f39b60c3d
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-05-15 19:34:11 +0400

    Has been changed getUser function

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,23 +3,26 @@ const config = require('config');
 
 const jwt = config.get('jwt');
 
-const { UserModel } = require('../models');
+const {UserModel} = require('../models');
 
-async function getUser (req, res, next) {
-  const { userId } = req.params;
-  const { authorization } = req.headers;
+async function getUser(req, res, next) {
+  const {userId} = req.params;
+  const {authorization} = req.headers;
 
   try {
     const decoded = await JWT.verify(authorization, jwt.secret);
 
-    if (userId !== decoded.id.toString()) {
+    if (userId !== decoded._id.toString()) {
       return res.status(401).json({
         error: 'Not Authorized'
       });
     }
 
-    const user = await UserModel.findOne({ _id: userId }).select({
-      password: 0
+    const user = await UserModel.findOne({_id: userId}).select({
+      password: 0,
+      email: 0,
+      mobilePhone: 0,
+      address: 0
     });
 
     return res.status(200).json({
@@ -28,7 +31,6 @@ async function getUser (req, res, next) {
   } catch (error) {
     return next(error);
   }
-
 }
 
 module.exports = {
--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUser } = require('../controllers/user-controller');
+const {getUser} = require('../controllers/user-controller');
 
 router.get('/:userId', getUser);
 
