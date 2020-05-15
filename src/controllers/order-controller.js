36806943668a03df042183e1704f79b93dda2525
Lilit <lilit@inone.am>
2020-05-15 18:35:46 +0400

    Limit and Offset paging parameters as query string params

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -2,10 +2,10 @@
 const { OrderModel } = require('../models');
 
 async function getOrders(req, res, next){
-  const { limit, offset } = req.params;
+  const { limit, offset } = req.query;
   try{
     return res.status(200).json({
-      orders: await OrderModel.find().limit(limit).skip(offset).exec()
+      orders: await OrderModel.find().limit(limit * 1).skip(offset * 1).exec()
     }); 
   }
   catch(error){
