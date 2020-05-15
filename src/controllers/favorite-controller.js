3a0e8d23dac4c85192559ca178e7ca5e45ce98a4
Lilit <lilit@inone.am>
2020-05-15 19:29:11 +0400

    Add endpoint for getting favourite products

--- src/controllers/favorite-controller.js
+++ src/controllers/favorite-controller.js
@@ -1,4 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 const { FavoritesModel } = require('../models');
+async function getFavorites(req, res, next){
+  const { limit, offset } = req.params;
+  try{
+    return res.status(200).json({
+      orders: await FavoritesModel.find().limit(limit).skip(offset).exec()
+    }); 
+  }
+  catch(error){
+    next(error);
+  }
+}
 
-module.exports = {};
+module.exports = {
+  getFavorites
+};
--- src/routers/favorite-router.js
+++ src/routers/favorite-router.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { getFavorits } = require('../controllers/favorite-controller');
+
+router.get('/products', getFavorits);
+
 module.exports = router;
