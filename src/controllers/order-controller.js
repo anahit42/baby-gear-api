edebb0275a9de806dfee342c3eea60ac9e20842e
Lilit <lilit@inone.am>
2020-05-15 18:24:15 +0400

    Add endpoint for getting single order

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -1,4 +1,20 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 const { OrderModel } = require('../models');
 
-module.exports = {};
+async function getOrder(req, res, next){
+  const { orderId } = req.params;
+  try{
+    return res.status(200).json({
+      orders: await OrderModel.findOne({
+        _id: orderId
+      })
+    }); 
+  }
+  catch(error){
+    next(error);
+  }
+}
+
+module.exports = {
+  getOrder
+};
--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { getOrder } = require('../controllers/order-controller');
+
+router.get('/:orderId', getOrder);
+
 module.exports = router;
