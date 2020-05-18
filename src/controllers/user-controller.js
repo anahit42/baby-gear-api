14a05633ac2223209a89ec4700185429a9947edd
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-05-18 23:17:35 +0400

    Has been added total for users, limit and skip are get from url

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,11 +3,11 @@ const config = require('config');
 
 const jwt = config.get('jwt');
 
-const {UserModel} = require('../models');
+const { UserModel } = require('../models');
 
 async function getUser(req, res, next) {
-  const {userId} = req.params;
-  const {authorization} = req.headers;
+  const { userId } = req.params;
+  const { authorization } = req.headers;
 
   try {
     const decoded = await JWT.verify(authorization, jwt.secret);
@@ -18,7 +18,7 @@ async function getUser(req, res, next) {
       });
     }
 
-    const user = await UserModel.findOne({_id: userId}).select({
+    const user = await UserModel.findOne({ _id: userId }).select({
       password: 0,
       email: 0,
       mobilePhone: 0,
@@ -34,13 +34,26 @@ async function getUser(req, res, next) {
 }
 
 async function getUsers(req, res, next) {
+  let { limit } = 0;
+  let { skip } = 0;
+
+  for (let key in req.query) {
+    limit = parseInt(req.query.limit);
+    skip = parseInt(req.query.skip);
+    console.log(key, req.query[key]);
+  }
+
   try {
     const users = await UserModel.find({}).select({
       password: 0,
       email: 0,
       mobilePhone: 0,
       address: 0
-    }).limit(2).skip(2);
+    }).limit(limit).skip(skip);
+
+    const total = await UserModel.countDocuments({});
+
+    users.push({ 'total': total });
 
     return res.status(200).json({
       users
