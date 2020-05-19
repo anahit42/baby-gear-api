3a3f2e196d78c46e7e6fe984fec040667a077519
Lilit <lilit@inone.am>
2020-05-19 19:00:46 +0400

    Add endpoint for removing favourite product

--- src/controllers/favorite-controller.js
+++ src/controllers/favorite-controller.js
@@ -1,4 +1,41 @@
-// eslint-disable-next-line no-unused-vars
 const { FavoritesModel } = require('../models');
+const NotfoundError = require('../errors/not-found-error');
 
-module.exports = {};
+async function deleteFavorite(req, res, next){
+  const { productId }  = req.params;
+  const userId = res.userData._id;
+  try {
+    
+    const product = await FavoritesModel.findOne({
+      userId
+    }, (err, doc)=>{
+      if(err)
+        return next(err);
+      let found = false;
+      for(let i in doc.products){
+        let id = doc.products[i]; 
+        if(id == productId){
+          found = true;
+          doc.products.splice(i, 1);
+          break;
+        }
+      }
+
+      if(!found)
+        return next(new NotfoundError('Item not found'));
+        
+      doc.save();
+    });
+
+    return res.status(200).json(product);
+
+  }
+  catch (error) {
+    return next(error);
+  }
+
+}
+
+module.exports = {
+  deleteFavorite
+};
--- src/routers/favorite-router.js
+++ src/routers/favorite-router.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const { validateProductId, validateProductOwner } = require('../controllers/favorite-controller');
+const { deleteFavorite } = require('../controllers/favorite-controller');
+
+router.delete('products/:productId', validateProductId, validateProductOwner, deleteFavorite);
+
 module.exports = router;
