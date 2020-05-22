179506e51daa0df4914398b23aa77e1c6070e4b2
Anahit Vardanyan <vardanian.anahit@gmail.com>
2020-05-22 18:59:05 +0400

    Fix validateProductId() function

--- src/middlewares/validators/favorite-validator.js
+++ src/middlewares/validators/favorite-validator.js
@@ -1,4 +1,5 @@
 const config = require('config');
+
 const { FavoriteSchemas } = require('./schemas');
 const { handleErrorDetails } = require('./handlers');
 
--- src/middlewares/validators/product-validator.js
+++ src/middlewares/validators/product-validator.js
@@ -1,9 +1,9 @@
 const config = require('config');
-const Joi = require('@hapi/joi');
 
-const validationOptions = config.get('validation.options');
 const { handleErrorDetails } = require('./handlers');
 const { ProductSchemas } = require('./schemas');
+
+const validationOptions = config.get('validation.options');
 
 function validateCreateProduct(req, res, next) {
   const { error } = ProductSchemas.productsCreateSchema.validate(req, validationOptions);
@@ -16,7 +16,7 @@ function validateCreateProduct(req, res, next) {
 }
 
 function validateUpdateProduct(req, res, next) {
-  const { error } = ProductSchemas.productsUpdateSchema.validate(req, validationOptions);
+  const { error } = ProductSchemas.productIdSchemas.validate(req, validationOptions);
 
   if (error) {
     return handleErrorDetails(error, next);
@@ -26,13 +26,7 @@ function validateUpdateProduct(req, res, next) {
 }
 
 function validateProductId(req, res, next) {
-  const { productId } = req.params;
-
-  const schema = Joi.object().keys({
-    productId: Joi.string().hex().length(24).required(),
-  });
-
-  const { error } = schema.validate({ productId });
+  const { error } = ProductSchemas.productsUpdateSchema.validate(req, validationOptions);
 
   if (error) {
     return handleErrorDetails(error, next);
