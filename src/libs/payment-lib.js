b5d238c1d91cfc333bc042b07ae1612b9f562395
Sona Israyelyan <sisrayelyan@worldbankgroup.org>
2020-05-29 20:14:14 +0400

    Create a payment method lib with getUserMethod function

--- src/libs/payment-lib.js
+++ src/libs/payment-lib.js
@@ -1,28 +1,27 @@
 const { PaymentMethodModel } = require('../models');
-const { NotFoundError } = require('../errors')
+const { NotFoundError } = require('../errors');
 
-async function getUserPaymentMethod(paymentMethodId) {
+async function getUserPaymentMethod(req) {
     try {
-
-        //const { paymentMethodId } = req.body
-        //const userId = req.userData._id;
+        const { paymentMethodId } = req.body;
+        const userId = req.userData._id;
 
         if (paymentMethodId) {
-            let paymentMethod = await PaymentMethodModel.findOne({ _id: paymentMethodId });
-            return paymentMethod
+            const paymentMethod = await PaymentMethodModel.findOne({ _id: paymentMethodId });
+            return paymentMethod;
         }
-        else {
-            paymentMethod = await PaymentMethodModel.findOne({ userId: userId }).where({ 'default': 'true' })
-            if (!paymentMethod) {
-                throw new NotFoundError('Default payment method not found, please provide payment method');
-            }
-            return paymentMethod
+        
+
+        const paymentMethod = await PaymentMethodModel.findOne({ userId }).where({ default: 'true' });
+        if (!paymentMethod) {
+            throw new NotFoundError('Default payment method not found, please provide payment method');
         }
+        return paymentMethod;
     } catch (error) {
-        console.log(error)
+        throw new Error();
     }
 }
 
 module.exports = {
-    getUserPaymentMethod
-}
+    getUserPaymentMethod,
+};
