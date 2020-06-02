0f2c37f205d7ff39c58cbd2ddaa61978904857ee
Admin <avetisyannarine@gmail.com>
2020-06-02 14:22:48 +0400

    review fixed

--- src/controllers/bucket-controller.js
+++ src/controllers/bucket-controller.js
@@ -5,57 +5,42 @@ const { ResponseHandlerUtil } = require('../utils');
 
 async function addProductToBucket(req, res, next) {
   const { productId, quantity } = req.body;
+  const { userId: paramsUserId } = req.params;
   const userId = req.userData._id;
+
+  if (paramsUserId !== userId) {
+    throw new ValidationError('User is not allowed to do this action');
+  }
 
   try {
     const product = await ProductModel.findOne({ _id: productId });
-    const increaseAmount = product.price * quantity;
-
-    let bucketData;
-
     if (!product) {
       throw new NotFoundError('Product not found');
     }
+
     if (product.quantity < quantity) {
       throw new ValidationError('Product not available in that quantity');
     }
 
     const currentBucketData = await BucketModel.findOne({ userId });
-    const pickedProduct = currentBucketData.products.find(
-      (prod) => prod.productId.toString() === productId.toString()
-    );
-
-    if (pickedProduct) {
-      bucketData = await BucketModel.findOneAndUpdate(
-        { userId, 'products.productId': pickedProduct.productId },
-        {
-          $set: {
-            'products.$.quantity': pickedProduct.quantity + quantity,
-          },
-          $inc: {
-            totalPrice: increaseAmount,
-          },
-        },
-        { new: true }
-      );
-    } else {
-      bucketData = await BucketModel.findOneAndUpdate(
-        { userId },
-        {
-          $addToSet: {
-            products: { productId, quantity },
-          },
-          $inc: {
-            totalPrice: increaseAmount,
-          },
-        },
-        { new: true }
-      );
-    }
-
-    if (!bucketData) {
+    if (!currentBucketData) {
       throw new NotFoundError('Not found');
     }
+
+    const pickedProduct = currentBucketData.products.find(
+      (prod) => prod.productId.toString() === productId
+    );
+
+    const increaseAmount = product.price * quantity;
+    if (pickedProduct) {
+      pickedProduct.quantity += quantity;
+      currentBucketData.totalPrice += increaseAmount;
+    } else {
+      currentBucketData.products.push({ productId, quantity });
+      currentBucketData.totalPrice += increaseAmount;
+    }
+
+    const bucketData = await currentBucketData.save();
 
     return ResponseHandlerUtil.handleCreate(res, bucketData);
   } catch (error) {
